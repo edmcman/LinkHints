8f8c4aaebbf9397b3eb5f39ea57050810a76202f
Edward J. Schwartz <edmcman@cmu.edu>
2025-12-30 09:01:18 -0500

    Basic testing

---
+++ playwright.config.ts
@@ -0,0 +1,22 @@
+import type { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  testDir: "tests",
+  timeout: 30_000,
+  expect: {
+    timeout: 10_000,
+  },
+  reporter: "list",
+  projects: [
+    {
+      name: "firefox",
+      use: {
+        browserName: "firefox",
+        // extensions need to be loaded in headed mode
+        headless: false,
+      },
+    },
+  ],
+};
+
+export default config;
---
+++ tests/firefox-tutorial.spec.ts
@@ -0,0 +1,32 @@
+import path from "path";
+
+import { Page, BrowserContext } from "playwright";
+import { createFixture } from "playwright-webextext";
+
+const TUTORIAL_WAIT_MS = 1_000;
+
+const tutorialUrl = "https://lydell.github.io/LinkHints/tutorial.html";
+
+const extensionPath = path.resolve(__dirname, "..", "compiled");
+
+const { test, expect } = createFixture(extensionPath);
+
+test("detect injected elements", async ({ context }: { context: BrowserContext }) => {
+    // Wait for the tutorial page to load.
+    await new Promise((r) => setTimeout(r, TUTORIAL_WAIT_MS));
+
+    // Now manually open the tutorial page
+    const page = await context.newPage();
+    await page.goto(tutorialUrl);
+    await page.waitForLoadState("load");
+
+    console.log("open tabs:", context.pages().map((p) => p.url()));
+    expect(page.url()).toBe(tutorialUrl);
+
+    // Press the keyboard shortcut to activate hints mode
+    await page.keyboard.press('Alt+j');
+
+    // Wait for the extension's renderer container to appear
+    await page.waitForSelector('#__LinkHintsWebExt', { timeout: 5000 });
+});
+
